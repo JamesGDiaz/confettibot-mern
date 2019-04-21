c84d876ed4c9fb4436ffd395111e7d0425fa7d57
Franco Giordani <jgdames@gmail.com>
2019-04-21 03:54:16 -0500

    asdasd

--- server/src/server/server.js
+++ server/src/server/server.js
@@ -1,4 +1,4 @@
-'use strict'
+"use strict";
 
 const {
   config,
@@ -9,13 +9,13 @@ const {
   show,
   websocket,
   stats
-} = require('../config')
-const routes = require('../../routes')
-const mongoose = require('mongoose')
+} = require("../config");
+const routes = require("../../routes");
+const mongoose = require("mongoose");
 // const spdy = require("spdy");
-const http = require('http')
-const Url = require('url-parse')
-let server = null
+const http = require("http");
+const Url = require("url-parse");
+let server = null;
 
 /**
  * Start HTTP/2 server, database, socket.io connection
@@ -23,63 +23,63 @@ let server = null
  * @function
  */
 const listen = () => {
-  const app = express.init()
-  session.init(app)
-  passport.init(app)
-  db.init()
-  server = http.createServer(app).listen(config.port)
-  show.debug(`Listening at http://${config.host}:${config.port}`)
-  routes.init(app)
-  websocketConfig(server)
-  stats.memory()
-}
+  const app = express.init();
+  session.init(app);
+  passport.init(app);
+  db.init();
+  server = http.createServer(app).listen(config.port);
+  show.debug(`Listening at http://${config.host}:${config.port}`);
+  routes.init(app);
+  websocketConfig(server);
+  stats.memory();
+};
 
 /**
  * Close server, database connection
  * @function
  */
 const close = () => {
-  server.close()
-  mongoose.disconnect()
-  show.debug('Server down')
-}
+  server.close();
+  mongoose.disconnect();
+  show.debug("Server down");
+};
 
 /**
  * Websocket endpoints configuration
  * @function
  */
 const websocketConfig = server => {
-  server.on('upgrade', function upgrade (request, socket, head) {
-    const pathname = new Url(request.url).pathname
-    if (pathname === '/api/app') {
-      websocket.wssApp.handleUpgrade(request, socket, head, function done (ws) {
-        websocket.wssApp.emit('connection', ws, request)
-      })
-    } else if (pathname === '/api/admin/mobile') {
+  server.on("upgrade", function upgrade(request, socket, head) {
+    const pathname = new Url(request.url).pathname;
+    if (pathname === "/api/app") {
+      websocket.wssApp.handleUpgrade(request, socket, head, function done(ws) {
+        websocket.wssApp.emit("connection", ws, request);
+      });
+    } else if (pathname === "/api/admin/mobile") {
       websocket.wssMobileAdmin.handleUpgrade(
         request,
         socket,
         head,
-        function done (ws) {
-          websocket.wssMobileAdmin.emit('connection', ws, request)
+        function done(ws) {
+          websocket.wssMobileAdmin.emit("connection", ws, request);
         }
-      )
-    } else if (pathname === '/api/admin/relay') {
+      );
+    } else if (pathname === "/api/admin/relay") {
       websocket.wssRelayAdmin.handleUpgrade(
         request,
         socket,
         head,
-        function done (ws) {
-          websocket.wssRelayAdmin.emit('connection', ws, request)
+        function done(ws) {
+          websocket.wssRelayAdmin.emit("connection", ws, request);
         }
-      )
+      );
     } else {
-      socket.destroy()
+      socket.destroy();
     }
-  })
-}
+  });
+};
 
 module.exports = {
   listen,
   close
-}
+};
