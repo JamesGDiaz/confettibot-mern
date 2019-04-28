9158c760d69c46d2512589d7cb9614418fafd2c1
Franco Giordani <jgdames@gmail.com>
2019-04-28 03:35:40 -0500

    verify log in, secure cookie

--- server/src/config/services/session.js
+++ server/src/config/services/session.js
@@ -1,40 +1,40 @@
-'use strict'
+"use strict";
 
-const crypto = require('crypto')
-const session = require('express-session')
-const mongoose = require('mongoose')
-const MongoStore = require('connect-mongo')(session)
+const crypto = require("crypto");
+const session = require("express-session");
+const mongoose = require("mongoose");
+const MongoStore = require("connect-mongo")(session);
 
 const options = {
   mongooseConnection: mongoose.connection
-}
-const mongoStore = new MongoStore(options)
+};
+const mongoStore = new MongoStore(options);
 
 const sessionParser = session({
   store: mongoStore,
-  secret: crypto.randomBytes(48).toString('hex'),
+  secret: crypto.randomBytes(48).toString("hex"),
   resave: false,
   saveUninitialized: false,
   rolling: true,
   cookie: {
-    secure: false,
+    secure: auto,
     httpOnly: true,
     maxAge: 60 * 60 * 1000 // 1hr
   }
-})
+});
 
 /**
  * Initialize mongo for session cache
  */
 const init = app => {
-  if (app.get('env') === 'production') {
-    app.set('trust proxy', 1) // trust first proxy
-    sessionParser.cookie.secure = true // serve secure cookies
+  if (process.env.NODE_ENV === "production") {
+    app.set("trust proxy", 1); // trust first proxy
+    sessionParser.cookie.secure = true; // serve secure cookies
   }
-  app.use(sessionParser)
-}
+  app.use(sessionParser);
+};
 
 module.exports = {
   init,
   sessionParser
-}
+};
--- server/src/websocket/websocket.js
+++ server/src/websocket/websocket.js
@@ -1,88 +1,89 @@
-const PythonShell = require('python-shell').PythonShell
-const SocketServer = require('ws').Server
-const config = require('../config/services/config')
-const sessionParser = require('../config/services/session').sessionParser
+const PythonShell = require("python-shell").PythonShell;
+const SocketServer = require("ws").Server;
+const config = require("../config/services/config");
+const sessionParser = require("../config/services/session").sessionParser;
 
-const wssAppServer = new SocketServer({ noServer: true, maxPayload: 512000 })
+const wssAppServer = new SocketServer({ noServer: true, maxPayload: 512000 });
 const wssApp = new SocketServer({
   noServer: true,
-  verifyClient: function (info, done) {
+  verifyClient: function(info, done) {
     sessionParser(info.req, {}, () => {
-      if (info.req.session.passport) done(info.req.session.passport.user)
-    })
+      if (info.req.session.passport && info.req.session.passport.user)
+        done(info.req.session.passport.user);
+    });
   }
-})
+});
 
 // Setup websocket for complete process ( search -> broadcast answer)
-wssApp.on('connection', (ws, req) => {
-  console.log('Connection to /api/app accepted.')
-  ws.on('message', data => {
-    data = JSON.parse(data)
-    if (data.type === 'AUTH') {
-      console.log(`Authenticating ID ${data.user.id}`)
-    } else ws.close()
-  })
-  let ip = null
-  if (process.env.NODE_ENV === 'production') {
-    ip = req.headers['x-forwarded-for'].split(/\s*,\s*/)[0]
-  } else ip = req.connection.remoteAddress
+wssApp.on("connection", (ws, req) => {
+  console.log("Connection to /api/app accepted.");
+  ws.on("message", data => {
+    data = JSON.parse(data);
+    if (data.type === "AUTH") {
+      console.log(`Authenticating ID ${data.user.id}`);
+    } else ws.close();
+  });
+  let ip = null;
+  if (process.env.NODE_ENV === "production") {
+    ip = req.headers["x-forwarded-for"].split(/\s*,\s*/)[0];
+  } else ip = req.connection.remoteAddress;
   console.log(
     `[IP: ${ip}] Client connected on /api/app, there are ${
       wssApp.clients.size
     } clients connected.`
-  )
-  ws.send('{"type": "INFO", "message": "Conectado!"}')
-  ws.on('close', () => {
+  );
+  ws.send('{"type": "INFO", "message": "Conectado!"}');
+  ws.on("close", () => {
     console.log(
       `[IP: ${ip}] Client disconnected from /api/app. There are ${
         wssApp.clients.size
       } clients connected.`
-    )
-  })
-})
+    );
+  });
+});
 
-wssAppServer.on('connection', (ws, req) => {
-  let ip = null
-  if (process.env.NODE_ENV === 'production') {
-    ip = req.headers['x-forwarded-for'].split(/\s*,\s*/)[0]
-  } else ip = req.connection.remoteAddress
+wssAppServer.on("connection", (ws, req) => {
+  let ip = null;
+  if (process.env.NODE_ENV === "production") {
+    ip = req.headers["x-forwarded-for"].split(/\s*,\s*/)[0];
+  } else ip = req.connection.remoteAddress;
   console.log(
     `[IP: ${ip}] Client connected to /api/app/server. There are ${
       wssAppServer.clients.size
     } clients connected.`
-  )
-  ws.send('{"type": "INFO", "message": "Conectado!"}')
-  ws.on('message', data => {
-    let uri = data.toString('base64')
+  );
+  ws.send('{"type": "INFO", "message": "Conectado!"}');
+  ws.on("message", data => {
+    let uri = data.toString("base64");
     var options = {
-      mode: 'text',
-      pythonPath: 'python3',
-      pythonOptions: ['-u'],
+      mode: "text",
+      pythonPath: "python3",
+      pythonOptions: ["-u"],
       args: [uri],
       scriptPath: config.pythonScriptsFolder
-    }
-    let pyconfettibot = new PythonShell('main.py', options)
-    pyconfettibot.on('message', message => {
+    };
+    let pyconfettibot = new PythonShell("main.py", options);
+    pyconfettibot.on("message", message => {
       wssApp.clients.forEach(client => {
-        client.send(message)
-      })
-      ws.send(message)
-      console.log(message)
-    })
-    pyconfettibot.end(function (err, code, signal) {
-      if (err && !(config.env === 'production')) console.log(err)
-    })
-  })
-  ws.on('close', () => {
+        client.send(message);
+      });
+      ws.send(message);
+      console.log(message);
+    });
+    pyconfettibot.end(function(err, code, signal) {
+      if (err && !(config.env === "production")) console.log(err);
+    });
+  });
+  ws.on("close", () => {
     console.log(
       `[IP: ${ip}] Client disconnected from /api/app/server. There are ${
         wssAppServer.clients.size
       } clients connected.`
-    )
-  })
-})
+    );
+  });
+});
 
 module.exports = {
   wssAppServer,
   wssApp
-}
+};
