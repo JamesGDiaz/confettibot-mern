8120e1f572a848c9e82e185029d42fe85577ea96
Franco Giordani <jgdames@gmail.com>
2019-05-02 03:09:41 -0500

    scure cookie

--- server/src/config/services/session.js
+++ server/src/config/services/session.js
@@ -1,40 +1,45 @@
-'use strict'
+"use strict";
 
-const crypto = require('crypto')
-const session = require('express-session')
-const mongoose = require('mongoose')
-const MongoStore = require('connect-mongo')(session)
+const crypto = require("crypto");
+const session = require("express-session");
+const mongoose = require("mongoose");
+const MongoStore = require("connect-mongo")(session);
 
 const options = {
   mongooseConnection: mongoose.connection
-}
-const mongoStore = new MongoStore(options)
+};
+const mongoStore = new MongoStore(options);
 
+let secure;
+if (process.env.NODE_ENV === "production") {
+  secure = true;
+} else {
+  secure = false;
+}
 const sessionParser = session({
   store: mongoStore,
-  secret: crypto.randomBytes(48).toString('hex'),
+  secret: crypto.randomBytes(48).toString("hex"),
   resave: false,
   saveUninitialized: false,
   rolling: true,
   cookie: {
-    secure: 'auto',
+    secure: secure,
     httpOnly: true,
     maxAge: 60 * 60 * 1000 // 1hr
   }
-})
+});
 
 /**
  * Initialize mongo for session cache
  */
 const init = app => {
-  if (process.env.NODE_ENV === 'production') {
-    app.set('trust proxy', 1) // trust first proxy
-    sessionParser.cookie.secure = true
+  if (process.env.NODE_ENV === "production") {
+    app.set("trust proxy", 1); // trust first proxy
   }
-  app.use(sessionParser)
-}
+  app.use(sessionParser);
+};
 
 module.exports = {
   init,
   sessionParser
-}
+};
