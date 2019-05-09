3e7c1e2935e1b563ce1f2868104a3206ef839b34
Franco Giordani <jgdames@gmail.com>
2019-05-08 20:30:40 -0500

    asdasd

--- server/webpack.config.js
+++ server/webpack.config.js
@@ -25,10 +25,6 @@ module.exports = {
       {
         from: "../client/build",
         to: "../build/client"
-      },
-      {
-        from: "./src/python_scripts/",
-        to: "../build/python_scripts"
       }
     ])
   ]
