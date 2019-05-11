37a9e594518dae63ecb72e97ffe521dc485e7780
Franco Giordani <jgdames@gmail.com>
2019-05-11 16:30:26 -0500

    routes

--- server/routes/home.js
+++ server/routes/home.js
@@ -2,8 +2,10 @@
 
 const express = require('express')
 const home = require('../src/home')
+const user = require('../src/user')
 let router = express.Router()
 
 router.get('*', home.index)
+router.post('/api/activation', user.activation)
 
 module.exports = router
